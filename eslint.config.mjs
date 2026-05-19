09d1a661b24d4402eeb25d5a80ac477e3548dce8
Eduardo Martínez <eduardoleolim@hotmail.com>
2026-05-19 13:03:20 -0600

    chore: examples added to pnpm workspace

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['node_modules', '**/dist', 'examples'] },
+  { ignores: ['**/node_modules', '**/dist', 'examples'] },
   { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   { languageOptions: { globals: { ...globals.node } } },
   eslint.configs.recommended,
